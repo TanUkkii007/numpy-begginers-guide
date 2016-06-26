f0a616ef75edd158c84f627b1e88b7fb3fce3639
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2016-06-26 21:15:00 +0900

    add ema.py

---
+++ ch03/ema.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(5)
+print("Exp", np.exp(x))
+print("Linspace", np.linspace(-1, 0, 5))
+
+N = 5
+weights = np.exp(np.linspace(-1., 0., N))
+
+weights /= weights.sum()
+print("Weights", weights)
+
+c = np.loadtxt('data.csv', delimiter=',', usecols=(6,), unpack=True)
+ema = np.convolve(weights, c)[N-1:-N+1]
+t = np.arange(N-1, len(c))
+
+plt.plot(t, c[N-1:], lw=1.0, label='Data')
+plt.plot(t, ema, '--', lw=2.0, label='Exponential Moving Average')
+plt.title('5 Days Exponential Moving Average')
+plt.xlabel('Days')
+plt.ylabel('Price ($)')
+plt.legend()
+plt.grid()
+plt.show()
